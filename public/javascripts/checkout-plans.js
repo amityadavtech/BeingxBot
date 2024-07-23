4499718888f4b0f46b7870d26380a1628a35dce6
Amit Yadav <vy9172631@gmail.com>
2024-07-23 20:36:14 +0530

    responsiv add

--- public/javascripts/checkout-plans.js
+++ public/javascripts/checkout-plans.js
@@ -21,7 +21,7 @@ span.onclick = function () {
 
 window.onclick = function (event) {
     if (event.target === modal) {
-        modal.style.display = "none";
+        modal.style.display = "flex";
     }
 }
 
