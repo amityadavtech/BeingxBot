43b2df4f4c2b834f2925d4026d88680fb3cdae72
Amit Yadav <vy9172631@gmail.com>
2024-07-24 16:53:29 +0530

    new emprove

--- routes/index.js
+++ routes/index.js
@@ -7,27 +7,27 @@ router.get('/', function (req, res, next) {
 });
 
 /* GET about page. */
-router.get('/about', function (req, res, next) {
+router.get('/about/', function (req, res, next) {
   res.render('about', { title: 'Express' });
 });
 
 /* GET service page. */
-router.get('/services', function (req, res, next) {
+router.get('/services/', function (req, res, next) {
   res.render('services', { title: 'Express' });
 });
 
-router.get('/checkout-plans', function (req, res, next) {
+router.get('/checkout-plans/', function (req, res, next) {
   res.render('checkout-plans', { title: 'Express' });
 });
 
 
 /* GET contact page. */
-router.get('/contact', function (req, res, next) {
+router.get('/contact/', function (req, res, next) {
   res.render('contact', { title: 'Express' });
 });
 
 /* GET request quote page. */
-router.get('/request-quote', function (req, res, next) {
+router.get('/request-quote/', function (req, res, next) {
   res.render('request-quote', { title: 'Express' });
 });
 
